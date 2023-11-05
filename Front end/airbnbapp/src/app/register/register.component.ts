5466f6ec9005c8cdfcd341866ad6c3a7201a70d9
Danijel Mohači <danijelmo9999@gmail.com>
2023-11-05 23:28:47 +0100

    dodao register stranicu izmenio login stranicu

--- Front end/airbnbapp/src/app/register/register.component.ts
+++ Front end/airbnbapp/src/app/register/register.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.form = this.fb.group({
+      username: [null, Validators.required],
+      email: [null, Validators.required],
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required],
+      country: [null, Validators.required],
+      city: [null, Validators.required],
+      streetName: [null, Validators.required],
+      streetNumber: [null, Validators.required],
+      password: [null, Validators.required],
+    });
+  }
+
+  submit() {}
 }
