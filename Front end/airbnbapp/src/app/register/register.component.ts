c521ebfc356538081b6bfd510e4a22dc4285adf5
vndrija <matijasevicicon@gmail.com>
2023-12-03 21:43:14 +0100

    fixed email val

--- Front end/airbnbapp/src/app/register/register.component.ts
+++ Front end/airbnbapp/src/app/register/register.component.ts
@@ -22,7 +22,7 @@ export class RegisterComponent {
   ) {
     this.form = this.fb.group({
       username: [null, Validators.required],
-      email: [Validators.required, Validators.email],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
       firstName: [null, Validators.required],
       lastName: [null, Validators.required],
       country: [null, Validators.required],
