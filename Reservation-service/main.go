f1d0829a8a61d5550a258ef6d5d3802bbfec9dde
Vukasin <vule2002.vc@gmail.com>
2023-12-04 23:49:44 +0100

    Implemented reservation repository with cassandra CRUD actions

--- Reservation-service/main.go
+++ Reservation-service/main.go
@@ -17,7 +17,6 @@ import (
 func main() {
 
 	port := os.Getenv("PORT")
-
 	if len(port) == 0 {
 		port = "8000"
 	}
--- Reservation-service/reservationRepository.go
+++ Reservation-service/reservationRepository.go
@@ -1,104 +1,205 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"time"
+	"os"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"github.com/gocql/gocql"
 )
 
 type ReservationRepo struct {
-	cli    *mongo.Client
-	logger *log.Logger
+	session *gocql.Session
+	logger  *log.Logger
 }
 
-func New(ctx context.Context, logger *log.Logger) (*ReservationRepo, error) {
-	// dburi := "mongodb+srv://mongo:mongo@cluster0.gdaah26.mongodb.net/?retryWrites=true&w=majority"
+func New(logger *log.Logger) (*ReservationRepo, error) {
+	db := os.Getenv("CASS_DB")
 
-	client, err := mongo.NewClient(options.Client().ApplyURI(dburi))
+	cluster := gocql.NewCluster(db)
+	cluster.Keyspace = "system"
+	session, err := cluster.CreateSession()
 	if err != nil {
+		logger.Println(err)
 		return nil, err
 	}
 
-	err = client.Connect(ctx)
+	err = session.Query(
+		fmt.Sprintf(`CREATE KEYSPACE IF NOT EXISTS %s
+					WITH replication = {
+						'class' : 'SimpleStrategy',
+						'replication_factor' : %d
+					}`, "reservation", 1)).Exec()
 	if err != nil {
+		logger.Println(err)
+	}
+	session.Close()
+
+	cluster.Keyspace = "reservation"
+	cluster.Consistency = gocql.One
+	session, err = cluster.CreateSession()
+	if err != nil {
+		logger.Println(err)
 		return nil, err
 	}
 
 	return &ReservationRepo{
-		cli:    client,
-		logger: logger,
+		session: session,
+		logger:  logger,
 	}, nil
 }
 
-func (uh *ReservationRepo) Disconnect(ctx context.Context) error {
-	err := uh.cli.Disconnect(ctx)
-	if err != nil {
-		return err
-	}
-	return nil
+func (rs *ReservationRepo) CloseSession() {
+	rs.session.Close()
 }
 
-func (pr *ReservationRepo) Ping() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+// func (pr *ReservationRepo) Ping() {
+// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+// 	defer cancel()
 
-	// Check connection -> if no error, connection is established
-	err := pr.cli.Ping(ctx, readpref.Primary())
+// 	// Check connection -> if no error, connection is established
+// 	err := pr.cli.Ping(ctx, readpref.Primary())
+// 	if err != nil {
+// 		pr.logger.Println(err)
+// 	}
+
+// 	// Print available databases
+// 	databases, err := pr.cli.ListDatabaseNames(ctx, bson.M{})
+// 	if err != nil {
+// 		pr.logger.Println(err)
+// 	}
+// 	fmt.Println(databases)
+// }
+
+func (rs *ReservationRepo) CreateTables() {
+	err := rs.session.Query(
+		fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s 
+					(acco_id UUID, reservation_id UUID, price int, date date, isDeleted bool
+					PRIMARY KEY ((acco_id, reservation_id), price)) 
+					WITH CLUSTERING ORDER BY (price ASC, date DESC)`,
+			"reservation_by_acco")).Exec()
 	if err != nil {
-		pr.logger.Println(err)
+		rs.logger.Println(err)
 	}
 
-	// Print available databases
-	databases, err := pr.cli.ListDatabaseNames(ctx, bson.M{})
+	err = rs.session.Query(
+		fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s 
+					(user_id UUID, reservation_id UUID, price int, date date, isDeleted bool
+					PRIMARY KEY ((user_id, reservation_id), price)) 
+					WITH CLUSTERING ORDER BY (price ASC, date DESC)`,
+			"reservation_by_user")).Exec()
 	if err != nil {
-		pr.logger.Println(err)
+		rs.logger.Println(err)
 	}
-	fmt.Println(databases)
+	if err != nil {
+		rs.logger.Println(err)
+	}
 }
 
-func (ur *ReservationRepo) Insert(patient *Reservation) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	patientsCollection := ur.getCollection()
+// -------Reservation By Accommodation-------//
+func (rs *ReservationRepo) GetReservationsByAcco(acco_id string) (ReservationsByAcco, error) {
+	scanner := rs.session.Query(`SELECT acco_id, reservation_id, price int, date date, isDeleted
+								FROM reservations_by_acco WHERE acco_id = ? AND isDeleted = 0`,
+		acco_id).Iter().Scanner()
 
-	result, err := patientsCollection.InsertOne(ctx, &patient)
-	if err != nil {
-		ur.logger.Println(err)
-		return err
+	var reservations ReservationsByAcco
+	for scanner.Next() {
+		var res ReservationByAcco
+		err := scanner.Scan(&res.AccoId, &res.ReservationId, &res.Price, &res.Date)
+		if err != nil {
+			rs.logger.Println(err)
+			return nil, err
+		}
+		reservations = append(reservations, &res)
 	}
-	ur.logger.Printf("Documents ID: %v\n", result.InsertedID)
-	return nil
-}
-
-func (pr *ReservationRepo) GetAll() (Reservations, error) {
-	// Initialise context (after 5 seconds timeout, abort operation)
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	reservationsCollection := pr.getCollection()
-	pr.logger.Println("Collection: ", reservationsCollection)
-
-	var reservations Reservations
-	reservationsCursor, err := reservationsCollection.Find(ctx, bson.M{})
-	if err != nil {
-		pr.logger.Println("Cant find reservationCollection: ", err)
-		return nil, err
-	}
-	if err = reservationsCursor.All(ctx, &reservations); err != nil {
-		pr.logger.Println("Reservation Cursor.All: ", err)
+	if err := scanner.Err(); err != nil {
+		rs.logger.Println(err)
 		return nil, err
 	}
 	return reservations, nil
 }
 
-func (pr *ReservationRepo) getCollection() *mongo.Collection {
-	patientDatabase := pr.cli.Database("mongoDemo")
-	patientsCollection := patientDatabase.Collection("reservations")
-	return patientsCollection
+func (rs *ReservationRepo) InsertReservationByAcco(resAcco *ReservationByAcco) error {
+	reservationId, _ := gocql.RandomUUID()
+	err := rs.session.Query(
+		`INSERT INTO reservations_by_acco (acco_id, reservation_id, price, date) 
+		VALUES (?, ?, ?, ?)`,
+		resAcco.AccoId, reservationId, resAcco.Price, resAcco.Date).Exec()
+	if err != nil {
+		rs.logger.Println(err)
+		return err
+	}
+	return nil
+}
+
+// -------Reservation By User-------//
+func (rs *ReservationRepo) GetReservationsByUser(user_id string) (ReservationsByUser, error) {
+	scanner := rs.session.Query(`SELECT user_id, reservation_id, price int, date date, isDeleted
+								FROM reservations_by_user WHERE user_id = ? AND isDeleted = 0`,
+		user_id).Iter().Scanner()
+
+	var reservations ReservationsByUser
+	for scanner.Next() {
+		var res ReservationByUser
+		err := scanner.Scan(&res.UserId, &res.ReservationId, &res.Price, &res.Date, &res.IsDeleted)
+		if err != nil {
+			rs.logger.Println(err)
+			return nil, err
+		}
+		reservations = append(reservations, &res)
+	}
+	if err := scanner.Err(); err != nil {
+		rs.logger.Println(err)
+		return nil, err
+	}
+	return reservations, nil
+}
+
+func (rs *ReservationRepo) InsertReservationByUser(resUser *ReservationByUser) error {
+	reservationId, _ := gocql.RandomUUID()
+	err := rs.session.Query(
+		`INSERT INTO reservations_by_user (user_id, reservation_id, price, date) 
+		VALUES (?, ?, ?, ?)`,
+		resUser.UserId, reservationId, resUser.Price, resUser.Date).Exec()
+	if err != nil {
+		rs.logger.Println(err)
+		return err
+	}
+	return nil
+}
+
+//--------------//
+
+func (rs *ReservationRepo) UpdateReservationByAcco(accoId string, reservationId string, price string, date string) error {
+	// za Update je neophodno da pronadjemo vrednost po PRIMARNOM KLJUCU = PK + CK (ukljucuje sve kljuceve particije i klastera)
+	// u ovom slucaju: PK = smerId, CK = student_id, indeks
+	err := rs.session.Query(
+		`UPDATE reservation_by_acco SET isDeleted = 1 where acoo_id = ? and reservation_id = ?`,
+		accoId, reservationId).Exec()
+	if err != nil {
+		rs.logger.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (rs *ReservationRepo) GetDistinctIds(idColumnName string, tableName string) ([]string, error) {
+	scanner := rs.session.Query(
+		fmt.Sprintf(`SELECT DISTINCT %s FROM %s`, idColumnName, tableName)).
+		Iter().Scanner()
+	var ids []string
+	for scanner.Next() {
+		var id string
+		err := scanner.Scan(&id)
+		if err != nil {
+			rs.logger.Println(err)
+			return nil, err
+		}
+		ids = append(ids, id)
+	}
+	if err := scanner.Err(); err != nil {
+		rs.logger.Println(err)
+		return nil, err
+	}
+	return ids, nil
 }
