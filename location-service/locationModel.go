2486336fa3cf795ac630e47dd136213564dd62af
Vukasin <vule2002.vc@gmail.com>
2023-12-21 15:47:01 +0100

    Organized folders

--- location-service/locationModel.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type Location struct {
-	ID           int    `json:"ID"`
-	Country      string `json:"country"`
-	City         string `json:"city"`
-	StreetName   string `json:"streetName"`
-	StreetNumber string `json:"streetNumber"`
-}
