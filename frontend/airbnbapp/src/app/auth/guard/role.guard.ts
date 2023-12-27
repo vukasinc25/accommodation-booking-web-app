c79653374f75ae7e586a7485cdbc82fcaf58e29a
Vukasin <vule2002.vc@gmail.com>
2023-12-28 00:51:53 +0100

    More organizing frontend...

--- frontend/airbnbapp/src/app/auth/guard/role.guard.ts
+++ frontend/airbnbapp/src/app/auth/guard/role.guard.ts
@@ -1,5 +1,5 @@
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../service/auth.service';
+import { AuthService } from '../../service/auth.service';
 import { inject } from '@angular/core';
 import { map } from 'rxjs';
 
--- frontend/airbnbapp/src/app/auth/password-email-recovery/password-email-recovery.component.ts
+++ frontend/airbnbapp/src/app/auth/password-email-recovery/password-email-recovery.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../service/auth.service';
+import { AuthService } from '../../service/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
